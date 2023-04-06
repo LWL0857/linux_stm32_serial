8427c187d34fa4a7ba81170bc7e5bf6feb75a4fe
“LWL0857” <18132855597@163.com>
2023-04-06 19:51:27 +0800

    修改注释

--- src/publish_node.cpp
+++ src/publish_node.cpp
@@ -25,7 +25,7 @@ void serial_callback(const geometry_msgs::PoseStamped::ConstPtr &msg)
     orientationZ = msg->pose.orientation.z;
     orientationW = msg->pose.orientation.w;
 
-    ROS_INFO("I heard the pose from the robot");
+    ROS_INFO("I heard the pose from mocap");
     ROS_INFO("the position(x,y,z) is %f , %f, %f", msg->pose.position.x, msg->pose.position.y, msg->pose.position.z);
     ROS_INFO("the orientation(x,y,z,w) is %f , %f, %f, %f", msg->pose.orientation.x, msg->pose.orientation.y, msg->pose.orientation.z, msg->pose.orientation.w);
     ROS_INFO("the time we get the pose is %f", msg->header.stamp.sec + 1e-9 * msg->header.stamp.nsec);
